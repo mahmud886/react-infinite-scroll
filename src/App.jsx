c795da1242c48f1dbbbc3076e18b0883570dc424
mahmud886 <iqbal886mahmud@gmail.com>
2024-02-26 14:19:40 +0600

    code added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductList from "./components/ProductList.jsx";
+
+function App() {
+
+  return (
+    <>
+      <div><h2 className="text-3xl text-red-700">React Infinite Scrolling...</h2></div>
+      <ProductList/>
+    </>
+  )
+}
+
+
+export default App
